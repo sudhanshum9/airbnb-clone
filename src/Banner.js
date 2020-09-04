7599a5e5d112576979635ebdef832beaed7bcaf1
sudhanshu mathur <sudhanshumathur01>
2020-09-04 12:44:26 +0530

    pushing the frontend of the website

---
+++ src/Banner.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import './Banner.css'
+import { Button } from '@material-ui/core'
+import Search from './Search'
+
+import { useHistory } from 'react-router-dom'
+
+function Banner() {
+
+    const history = useHistory(); 
+    const [showSearch, setShowSeach] = useState(false);
+
+
+
+    return (
+        <div className='banner'>
+            <div className='banner__search'>
+                {showSearch && < Search />}
+                <Button onClick={() => setShowSeach(!showSearch)} className='banner__searchButton'
+                    variant='outlined'>{showSearch ? 'Hide' : 'Search Dates'}</Button>
+            </div>
+            <div className='banner__info'>
+                <h5>Be a explorer</h5>
+                <Button onClick ={()=> history.push('/search')} variant='outlined'>Explore Nearby</Button>
+            </div>
+        </div>
+    )
+}
+
+export default Banner
---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Footer.css'
+function Footer() {
+    return (
+        <div className = 'footer'>
+            <p>@2020 Airbnb clone !!! NO right reseved --- This is a Demo!!!</p>
+        </div>
+    )
+}
+
+export default Footer

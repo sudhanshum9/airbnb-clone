a8225376ba2a063df6afa739b3ba5c1838a29ceb
sudhanshu mathur <sudhanshumathur01>
2020-10-08 14:39:20 +0530

    Updated the site and deplyed on firebase

--- src/Home.js
+++ src/Home.js
@@ -28,20 +28,20 @@ function Home() {
                 <Card
                     src="https://media.nomadicmatt.com/2019/airbnb_breakup3.jpg"
                     title="3 Bedroom Flat in Bournemouth"
-                    description="Superhost with a stunning view of the beachside in Sunny Bournemouth"
-                    price="£130/night"
+                    description="Superhost with a stunning view of the beachside in Sunny Goa"
+                    price="₹1300/night"
                 />
                 <Card
                     src="https://thespaces.com/wp-content/uploads/2017/08/Courtesy-of-Airbnb.jpg"
-                    title="Penthouse in London"
-                    description="Enjoy the amazing sights of London with this stunning penthouse"
-                    price="£350/night"
+                    title="Penthouse in Jaipur"
+                    description="Enjoy the amazing sights of Jaipur with this stunning penthouse"
+                    price="₹2500/night"
                 />
                 <Card
                     src="https://media.nomadicmatt.com/2018/apartment.jpg"
                     title="1 Bedroom apartment"
                     description="Superhost with great amenities and a fabolous shopping complex nearby"
-                    price="£70/night"
+                    price="₹700/night"
                 />
             </div>
         </div>
